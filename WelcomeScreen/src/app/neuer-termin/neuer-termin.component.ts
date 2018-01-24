403ad6970dcef9ff02c0a56ad03bcbcde84a04ee
Kenan Horoz <kenan.horoz@clear-it.de>
2018-01-24 13:14:39 +0100

    Neuer Termin aktualisiert

--- WelcomeScreen/src/app/neuer-termin/neuer-termin.component.ts
+++ WelcomeScreen/src/app/neuer-termin/neuer-termin.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-neuer-termin',
   templateUrl: './neuer-termin.component.html',
   styleUrls: ['./neuer-termin.component.css']
 })
 export class NeuerTerminComponent implements OnInit {
+
+
+  datum: string = "Datum";
+  uhrzeit: string = "Uhrzeit";
 
   constructor() { }
 
